0131f288f41d77307f8a88566d54cf5225cb7df3
liuyanjuntr-ltarctfp <64081662+liuyanjuntr-ltarctfp@users.noreply.github.com>
2020-04-23 22:08:05 +0800

    node-1 js
    案例添加

---
+++ node-1/js/08route.js
@@ -0,0 +1,31 @@
+//当用户访问  /student/1234567890查询学生的信息
+//当用户访问  /teacher/232323查询老师的信息
+let http = require('http');
+let url = require('url');
+let server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        return;
+    }
+    var userurl = req.url;
+    res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+    if (userurl.substr(0, 9) === "/student/") {
+        var studentid = userurl.substr(9);
+        if (/^\d{10}$/.test(studentid)) {
+            res.end("你所查询的学生信息，id是：" + studentid);
+        } else {
+            res.end("你所查询的学生信息错误，id是：" + studentid);
+        }
+    }
+    // teacher信息判断
+    if (userurl.substr(0, 9) === "/teacher/") {
+        var teacherid = userurl.substr(9);
+        if (/^\d{6}$/.test(teacherid)) {
+            res.end("你所查询的老师信息，id是：" + teacherid);
+        } else {
+            res.end("你所查询的老师信息错误，id是：" + teacherid);
+        }
+    }
+    res.end();
+})
+console.log('B');
+server.listen(5000, 'localhost');
